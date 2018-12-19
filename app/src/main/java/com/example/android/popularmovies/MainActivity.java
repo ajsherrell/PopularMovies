d5b0ce5c9d2dbf536a0820affb41aad5cd097253
AJ Sherrell <amiesherrell@gmail.com>
2018-12-19 18:46:54 -0500

    initial commit

---
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
